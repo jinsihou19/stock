d984b57330a44cde1997577e0e9119b1a9cef134
vito <vito@finereport.com>
2020-03-18 01:03:36 +0800

    Initial commit

---
+++ stock4.py
@@ -0,0 +1,26 @@
+# coding=utf-8
+import json
+import urllib2
+
+code = "000300.SS"
+url = "https://api-ddc-wscn.xuangubao.cn/market/trend?fields=tick_at,close_px,px_change,px_change_rate&prod_code={}".format(
+    code)
+data = urllib2.urlopen(url)
+obj = json.load(data)
+
+stock = obj["data"]["candle"][code]
+latest = stock["lines"][-1]
+pre_close_px = stock["pre_close_px"]
+close_px = latest[1]
+px_change = latest[2]
+px_change_rate = "%.2f%%" % (latest[3])
+if px_change > 0:
+    trend = "▲"
+    font_color = "255,96,94,255"
+else:
+    trend = "▼"
+    font_color = "122,174,61,255"
+
+res = {'text': "沪深300 " + str(close_px) + "(" + px_change_rate + ")" + trend, 'font_color': font_color}
+
+print(json.dumps(res))
