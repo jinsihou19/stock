7f8da12c383524b5c45e249c34f4f5f5c825effa
vito <vito@finereport.com>
2020-03-18 09:33:12 +0800

    bugfix: 修复lines为空的问题

--- stock.py
+++ stock.py
@@ -9,18 +9,23 @@ data = urllib2.urlopen(url)
 obj = json.load(data)
 
 stock = obj["data"]["candle"][code]
-latest = stock["lines"][-1]
+lines = stock["lines"]
 pre_close_px = stock["pre_close_px"]
-close_px = latest[1]
-px_change = latest[2]
-px_change_rate = "%.2f%%" % (latest[3])
-if px_change > 0:
-    trend = "▲"
-    font_color = "255,96,94,255"
+if len(lines):
+    latest = stock["lines"][-1]
+    close_px = latest[1]
+    px_change = latest[2]
+    px_change_rate = "%.2f%%" % (latest[3])
+    if px_change > 0:
+        trend = "▲"
+        font_color = "255,96,94,255"
+    else:
+        trend = "▼"
+        font_color = "122,174,61,255"
+    res = {'text': "上证 " + str(close_px) + "(" + px_change_rate + ")" + trend, 'font_color': font_color}
 else:
-    trend = "▼"
-    font_color = "122,174,61,255"
-
-res = {'text': "上证 " + str(close_px) + "(" + px_change_rate + ")" + trend, 'font_color': font_color}
+    close_px = pre_close_px
+    font_color = "255,255,255,255"
+    res = {'text': "上证 " + str(close_px), 'font_color': font_color}
 
 print(json.dumps(res))
